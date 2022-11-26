e1a96041c98eb394b203353c0ca134c59d39d6bd
SkateZen <105292058+SkateZen@users.noreply.github.com>
2022-11-26 13:43:11 +0100

    Assets Labyrinthe 2D

---
+++ Assets/Script/Constants.cs
@@ -0,0 +1,10 @@
+﻿
+public class Constants
+{
+    public const int MapWidth = 61;
+    public const int MapHeight = 61;
+
+    public const int CaseSize = 1;
+
+    public const float altitude = 60f;
+}
